ec78d29ce8eb7371ca046e1b30d15b509e70a285
shubham30102002 <shubhamrg30@gmail.com>
2024-03-22 14:23:32 +0530

    auth middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,14 +4,14 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 //signup route handler
-exports.signup = async(req,res) => {
-    try{
+exports.signup = async (req, res) => {
+    try {
         //get data
-        const {name,email, password, role} = req.body;
+        const { name, email, password, role } = req.body;
         //check if user already exist
-        const existingUser = await User.findOne({email});
+        const existingUser = await User.findOne({ email });
         //if user exist return 
-        if(existingUser){
+        if (existingUser) {
             return res.status(400).json({
                 success: false,
                 message: "User already exists"
@@ -20,25 +20,25 @@ exports.signup = async(req,res) => {
 
         //hashing password
         let hashPassword;
-        try{
-            hashPassword = await bcrypt.hash(password , 10);
-        }catch(err){
+        try {
+            hashPassword = await bcrypt.hash(password, 10);
+        } catch (err) {
             return res.status(500).json({
                 success: false,
                 message: "Error in hashing password"
             });
         }
-        
+
         //hw how to implement multiple try to hash password let say 3 times and then return error
 
 
         //create entry for user
-        const user = await User.create({name, email,password:hashPassword, role})
+        const user = await User.create({ name, email, password: hashPassword, role })
         return res.status(200).json({
             success: true,
             message: "User created successfully"
         });
-    }catch(error){
+    } catch (error) {
         console.error(error);
         return res.status(500).json({
             success: false,
@@ -49,68 +49,75 @@ exports.signup = async(req,res) => {
 
 
 
-exports.login = async(req,res) => {
-    try{    
+exports.login = async (req, res) => {
+    try {
         //data fetch
-        const {email,password} = req.body;
+        const { email, password } = req.body;
         //validation on email and password is empty or not
-        if(!email || !password){
-           return res.status(400).json({
-                success:false,
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
                 message: "Please fill all the details carefully"
             })
         }
-        
+
         //check if user is present in the database
-        let user = await User.findOne({email});
+        let user = await User.findOne({ email });
         //if not a register user
-        if(!user){
+        if (!user) {
             return res.status(500).json({
-                success:false,
+                success: false,
                 message: "User is not registered, please signup first."
             })
         }
 
         const payload = {
-            email : user.email,
+            email: user.email,
             id: user._id,
             role: user.role,
         };
         //verify password & generate JWT token
-        if(await bcrypt.compare(password, user.password)){
+        if (await bcrypt.compare(password, user.password)) {
             //password is match
-            let token = jwt.sign(payload, process.env.JWT_SECRET, 
-                                    {
-                                        expiresIn : "2h",
-                                    });
-            
+            let token = jwt.sign(payload, process.env.JWT_SECRET,
+                {
+                    expiresIn: "2h",
+                });
+
             // console.log(typeof(user));
-            // user = user.toObject();//why we need this?
+            user = user.toObject();//why we need this?
             user.token = token; //user ke andhar token banake usmein send kar diya
             user.password = undefined; // user ke object mein se password hata diya for security purpose
-                         
+
             const options = {
-                expires : new Date( Date.now() + 3*24*60*60*1000) , //persent se 3 days cookie with stay   )
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), //persent se 3 days cookie with stay   
                 httpOnly: true, //cannot be accessed on client side
 
             }
             //cookie mein data pass kiya 
-            res.cookie("ParleG" , token, options).status(200).json({
+            res.cookie("token", token, options).status(200).json({
                 success: true,
                 token,
                 user,
                 message: "User logged in successfully"
             })
 
-        }else{
+            // res.status(200).json({
+            //     success:true,
+            //     token,
+            //     user,
+            //     message:'User Logged in successfully',
+            // });
+
+        } else {
             //password do not match
             return res.status(403).json({
-                success:false,
+                success: false,
                 message: "Password incorrect!"
             })
         }
 
-    }catch(error){
+    } catch (error) {
         console.error(error);
         return res.status(500).json({
             success: false,
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,20 +1,27 @@
 //auth, isStudent, isAdmin
 
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-exports.auth = (req,res,next) => {
-    try{
+exports.auth = (req, res, next) => {
+    try {
         //extract jwt token
         //other ways to fetch token -> header, req body, cookies
 
-        // console.log("cookie ", req.cookies.token);
-        // console.log("body ", req.body.token);
-        // console.log("Header ", req.header("Authorization"));
-        const token = req.body.token || req.cookies.token || req.header("Authorization").replace("Bearer ","");
+        console.log("cookie ", req.cookies.token);
+        console.log("body ", req.body.token);
+        console.log("Header ", req.header("Authorization"));
+
+
+        // const token = req.body.token || req.cookies.token || req.header("Authorization").replace("Bearer ","");
         // console.log(token);
+        
+
+       const token  = req.cookies.token
+
+
         //token not found
-        if(!token) {
+        if (!token) {
             return res.status(400).json({
                 success: false,
                 message: "Token missing"
@@ -22,58 +29,58 @@ exports.auth = (req,res,next) => {
         }
 
         //verify token
-        try{
-            const decode = jwt.verify( token ,process.env.JWT_SECRET);
-            console.log(decode);
+        try {
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            // console.log(decode);
 
             req.user = decode; //store this is in user 
-        }catch(err) {
+        } catch (err) {
             return res.status(401).json({
                 success: false,
-                message:"Token is invalid!"
+                message: "Token is invalid!"
             })
         }
 
         next();
-    }catch(error){
+    } catch (error) {
         return res.status(401).json({
             success: false,
-            message:"Something went wrong while verifying the token"
+            message: "Something went wrong while verifying the token"
         })
     }
 }
 
 
-exports.isStudent =  (req,res,next) => {
-    try{
-        if(req.user.role !== "Student"){
+exports.isStudent = (req, res, next) => {
+    try {
+        if (req.user.role !== "Student") {
             return res.status(401).json({
                 success: false,
-                message:"This is a protected route for students"
+                message: "This is a protected route for students"
             })
         }
         next();
-    }catch(error){
+    } catch (error) {
         return res.status(500).json({
             success: false,
-            message:"User role is not matching"
+            message: "User role is not matching"
         })
     }
 }
 
-exports.isAdmin =  (req,res,next) => {
-    try{
-        if(req.user.role !== "Admin"){
+exports.isAdmin = (req, res, next) => {
+    try {
+        if (req.user.role !== "Admin") {
             return res.status(401).json({
                 success: false,
-                message:"This is a protected route for admin"
+                message: "This is a protected route for admin"
             })
         }
         next();
-    }catch(error){
+    } catch (error) {
         return res.status(500).json({
             success: false,
-            message:"User role is not matching"
+            message: "User role is not matching"
         })
     }
 }
